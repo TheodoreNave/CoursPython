2d59a00b03230d8ac38f76b781a468baf87bef06
Théodore Nave <theodoreyws@hotmail.fr>
2020-03-06 13:04:54 +0100

    started the for loop

--- Ex02.py
+++ Ex02.py
@@ -9,7 +9,7 @@ class animals:
         self.food = food
 
     def __str__(self):
-        return "Cet animal est un(e) " + self.name + " il possède " + str(self.nb) + " Jambes, il est de type " + self.aliment + ", il a " + str(self.nb_pattes) + " pattes et mange environ " + str(self.food) + " grammes de nourriture"
+        return "un animal qui est un(e) " + self.name + " il possède " + str(self.nb) + " Jambes, il est de type " + self.aliment + ", il a " + str(self.nb_pattes) + " pattes et mange environ " + str(self.food) + " grammes de nourriture"
 
 class animaux_marin(animals):
     pass
@@ -19,27 +19,28 @@ pieuvre = animaux_marin('Pieuvre', 1, 'Carnivore', 0, 200)
 class mammiferes(animals):
     pass
 
-        humain = mammiferes('Humain', 2, 'Omnivore', 2, 600)
-        lion = mammiferes('Lion', 2, 'Carnivore', 4, 3000)
-        mouton = mammiferes('Mouton', 5, 'Vegetarien', 4, 500)
-        lapin = mammiferes('Lapin', 7, 'Vegetarien', 4, 100)
-        chien = mammiferes('Chien', 2,  'Omnivore', 4, 500)
+humain = mammiferes('Humain', 2, 'Omnivore', 2, 600)
+lion = mammiferes('Lion', 2, 'Carnivore', 4, 3000)
+mouton = mammiferes('Mouton', 5, 'Vegetarien', 4, 500)
+lapin = mammiferes('Lapin', 7, 'Vegetarien', 4, 100)
+chien = mammiferes('Chien', 2,  'Omnivore', 4, 500)
 
 class domestique(animals):
     pass
 
-        poule = domestique('Poule', 8, 'Omnivore', 2, 2000)
-        mouton = domestique('Mouton', 5, 'Vegetarien', 4, 500)
-        lapin = domestique('Lapin', 7, 'Vegetarien', 4, 100)
-        chien = domestique('Chien', 2, 'Omnivore', 4, 500)
+poule = domestique('Poule', 8, 'Omnivore', 2, 2000)
+mouton = domestique('Mouton', 5, 'Vegetarien', 4, 500)
+lapin = domestique('Lapin', 7, 'Vegetarien', 4, 100)
+chien = domestique('Chien', 2, 'Omnivore', 4, 500)
 
 
 if __name__=="__main__":
 
-        autruche = animals('Autruche', 4, 'Omnivore', 2, 1000)
-        serpent = animals('Serpent', 2, 'Carnivore', 0, 200)
+    autruche = animals('Autruche', 4, 'Omnivore', 2, 1000)
+    serpent = animals('Serpent', 2, 'Carnivore', 0, 200)
 
-        ma_ferme = {}
-        ma_ferme["Dans ma ferme il y a :"] = animals()
+    ma_ferme = {}
+    ma_ferme["Poule"] = mammiferes
 
-    for
+    for key in ma_ferme:
+            print(key + "\n" + str(ma_ferme[key]))
